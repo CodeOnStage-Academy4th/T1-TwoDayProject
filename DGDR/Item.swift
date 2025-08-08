6e31a089f354cfab1fa9b63a3778ffeb4ca24095
pearhyunjin <pearhyunjin@gmail.com>
2025-08-09 03:16:04 +0900

    Initial Xcode project

---
+++ DGDR/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DGDR
+//
+//  Created by 배현진 on 8/9/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
